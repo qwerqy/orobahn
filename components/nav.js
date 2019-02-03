70767c7ed49862b63a3db3ef6d9ae7f8be5e2055
Amin Roslan <amnrsln@gmail.com>
2019-02-03 16:45:05 +0800

    make navbar responsive to width

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,5 @@
 import { Component, Fragment } from 'react';
-import { Menu, Segment, Transition } from 'semantic-ui-react';
+import { Menu, Segment, Transition, Responsive } from 'semantic-ui-react';
 
 import "../assets/css/nav.css"
 
@@ -8,19 +8,19 @@ const links = [
   { label: 'gaming', href: '/' }
 ]
 
-class Nav extends Component {
-  state = {
-    activeItem: ''
-  }
+const getWidth = () => {
+  const isSSR = typeof window === 'undefined'
 
-  handleClick = (e, { name }) => {
-    this.setState({ activeItem: name })
-  }
+  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
+}
 
+class DesktopNav extends Component {
+  handleClick = (e, {name}) => {
+    this.props.onMenuSelect(name)
+  }
+  
   render() {
-    const { show, fixed } = this.props
-    const { activeItem } = this.state
-
+    const { show, fixed, activeItem } = this.props
     const styles = {
       segment: {
         backgroundColor: show && fixed ? 'white' : 'transparent', 
@@ -34,25 +34,139 @@ class Nav extends Component {
         padding: '1em 20em'
       }
     }
+
     return (
       <Fragment>
-        <Transition visible={show} animation="fade down" duration={500}>
-          <Segment inverted textAlign='center' style={styles.segment}>
-            <Menu pointing secondary style={{ borderBottom: 0 }}>
-              <Menu.Item style={{letterSpacing: '3px'}} header>AMIN ROSLAN</Menu.Item>
-              {
-                Object.keys(links).map( i => {
-                  return (
-                    <Menu.Item key={i} name={links[i].label} active={activeItem === links[i].label} onClick={this.handleClick} position={links[i].position} />
-                  )
-                })
-              }
-            </Menu>
-          </Segment>
-        </Transition>
+        <Responsive getWidth={getWidth} minWidth={Responsive.onlyComputer.minWidth}>
+          <Transition visible={show} animation="fade down" duration={500}>
+            <Segment inverted textAlign='center' style={styles.segment}>
+              <Menu pointing secondary style={{ borderBottom: 0 }}>
+                <Menu.Item style={{letterSpacing: '3px'}} header>AMIN ROSLAN</Menu.Item>
+                {
+                  Object.keys(links).map( i => {
+                    return (
+                      <Menu.Item key={i} name={links[i].label} active={activeItem === links[i].label} onClick={this.handleClick} position={links[i].position} />
+                    )
+                  })
+                }
+              </Menu>
+            </Segment>
+          </Transition>
+        </Responsive>
       </Fragment>
     )
   }
 }
 
-export default Nav
+class TabletNav extends Component {
+  handleClick = (e, {name}) => {
+    this.props.onMenuSelect(name)
+  }
+  
+  render() {
+    const { show, fixed, activeItem } = this.props
+    const styles = {
+      segment: {
+        backgroundColor: show && fixed ? 'white' : 'transparent', 
+        border: 0, 
+        display: show ? 'block' : 'none',
+        position: show && fixed ? 'fixed' : 'absolute',
+        zIndex: '100',
+        margin: 0,
+        width: '100%',
+        top: 0,
+        padding: '1em 5em'
+      }
+    }
+
+    return (
+      <Fragment>
+        <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth} maxWidth={Responsive.onlyTablet.maxWidth}>
+          <Transition visible={show} animation="fade down" duration={500}>
+            <Segment inverted textAlign='center' style={styles.segment}>
+              <Menu pointing secondary style={{ borderBottom: 0 }}>
+                <Menu.Item style={{letterSpacing: '3px'}} header>AMIN ROSLAN</Menu.Item>
+                {
+                  Object.keys(links).map( i => {
+                    return (
+                      <Menu.Item key={i} name={links[i].label} active={activeItem === links[i].label} onClick={this.handleClick} position={links[i].position} />
+                    )
+                  })
+                }
+              </Menu>
+            </Segment>
+          </Transition>
+        </Responsive>
+      </Fragment>
+    )
+  }
+}
+
+class MobileNav extends Component {
+  handleClick = (e, {name}) => {
+    this.props.onMenuSelect(name)
+  }
+  
+  render() {
+    const { show, fixed, activeItem } = this.props
+    const styles = {
+      segment: {
+        backgroundColor: show && fixed ? 'white' : 'transparent', 
+        border: 0, 
+        display: show ? 'block' : 'none',
+        position: show && fixed ? 'fixed' : 'absolute',
+        zIndex: '100',
+        margin: 0,
+        width: '100%',
+        top: 0,
+        padding: '1em 0em'
+      }
+    }
+
+    return (
+      <Fragment>
+        <Responsive getWidth={getWidth} minWidth={Responsive.onlyMobile.minWidth} maxWidth={Responsive.onlyMobile.maxWidth}>
+          <Transition visible={show} animation="fade down" duration={500}>
+            <Segment inverted textAlign='center' style={styles.segment}>
+              <Menu pointing secondary style={{ borderBottom: 0, padding: 0 }}>
+                <Menu.Item style={{letterSpacing: '3px'}} header>AMIN ROSLAN</Menu.Item>
+                {
+                  Object.keys(links).map( i => {
+                    return (
+                      <Menu.Item key={i} name={links[i].label} active={activeItem === links[i].label} onClick={this.handleClick} position={links[i].position} />
+                    )
+                  })
+                }
+              </Menu>
+            </Segment>
+          </Transition>
+        </Responsive>
+      </Fragment>
+    )
+  }
+}
+
+class ResponsiveNav extends Component {
+  state = {
+    activeItem: ''
+  }
+
+  handleClick = (name) => {
+    this.setState({ activeItem: name })
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <DesktopNav {...this.props} {...this.state} onMenuSelect={this.handleClick}/>
+        <TabletNav {...this.props} {...this.state} onMenuSelect={this.handleClick}/>
+        <MobileNav {...this.props} {...this.state} onMenuSelect={this.handleClick}/>
+      </Fragment>
+
+    )
+  }
+}
+
+
+
+export default ResponsiveNav
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,22 @@
 import { Component } from 'react'
-import { Visibility } from 'semantic-ui-react'
+import { Visibility, Responsive } from 'semantic-ui-react'
 import Head from '../components/head'
 import Cover from '../components/cover'
 import HeroBox from '../components/herobox'
 import HeroPage from '../components/heropage'
 import Footer from '../components/footer'
 import Projects from '../components/projects'
-import Nav from '../components/nav'
-
-const links = [
-  {icon:"facebook", link:"https://www.facebook.com/amnrsln" },
-  {icon:"instagram", link:"https://www.instagram.com/taikomin/"},
-  {icon:"twitter", link:"https://twitter.com/qwerqy_dev"},
-  {icon:"linkedin", link:"https://www.linkedin.com/in/aminroslan/"},
-  {icon:"github", link:"https://www.github.com/qwerqy"}
-]
-
+import ResponsiveNav from '../components/nav'
 
 class Home extends Component {
   state = {
-    showNav: false
+    showNav: false,
+    width: 0
+  }
+  getWidth = () => {
+    const isSSR = typeof window === 'undefined'
+  
+    return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
   }
   showFixedMenu = () => {
     this.setState({ showNav: true })
@@ -29,23 +26,25 @@ class Home extends Component {
     return (
       <div>
         <Head title="Home" />
-        <Visibility once={false} onBottomPassed={() => this.setState({ showNav: true})} onBottomPassedReverse={() => this.setState({ showNav: false})}>
-          <Cover />
-        </Visibility>
-        <Nav show={this.state.showNav} fixed />
-        <HeroBox title='work' dark slant='right'>
-          I am a Full-Stack Software Engineer. I am currently with Vase Technologies as their Full-Stack Software Engineer. 
-          I am responsible in developing & maintaining their in-house products both for our business clients & consumer clients.
-          All of their products are mostly in Javascript.
-        </HeroBox>
-        <HeroBox title='game' titleAlign='right'>
-          I used to stream on Twitch going under the name 'GreenCheese'. I am currently putting streaming on hold in the hopes of getting better gears for it.
-          I like to play strategy, role-playing, heavy-story based games.
-        </HeroBox>
-        <HeroPage title='personal projects'>
-          <Projects />
-        </HeroPage>
-        <Footer />
+        <Responsive getWidth={this.getWidth}>
+          <Visibility once={false} onBottomPassed={() => this.setState({ showNav: true})} onBottomPassedReverse={() => this.setState({ showNav: false})}>
+            <Cover />
+          </Visibility>
+          <ResponsiveNav show={this.state.showNav} fixed />
+          <HeroBox title='work' dark slant='right'>
+            I am a Full-Stack Software Engineer. I am currently with Vase Technologies as their Full-Stack Software Engineer. 
+            I am responsible in developing & maintaining their in-house products both for our business clients & consumer clients.
+            All of their products are mostly in Javascript.
+          </HeroBox>
+          <HeroBox title='game' titleAlign='right'>
+            I used to stream on Twitch going under the name 'GreenCheese'. I am currently putting streaming on hold in the hopes of getting better gears for it.
+            I like to play strategy, role-playing, heavy-story based games.
+          </HeroBox>
+          <HeroPage title='personal projects'>
+            <Projects />
+          </HeroPage>
+          <Footer />
+        </Responsive>
       </div>
     )
   }
