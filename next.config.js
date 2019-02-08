75bd40ffcedacaaf7dae1f69a3922869f835648b
Amin Roslan <amnrsln@gmail.com>
2019-02-09 04:59:44 +0800

    added exportpathmap for slug support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withCSS = require('@zeit/next-css')
+const client = require('./client')
+
 require('dotenv').config()
 
 module.exports = withCSS({
@@ -12,5 +14,26 @@ module.exports = withCSS({
   },
   publicRuntimeConfig: {
     BUTTERCMS_API: process.env.BUTTERCMS_API
+  },
+  exportPathMap: async function (defaultPathMap) {
+  
+    const path = await client
+      // get all the posts and return those with slugs
+      .fetch('*[_type == "post"].slug.current')
+      .then(data =>
+        // use reduce to build an object with routes
+        // and select the blog.js file, and send in the
+        // correct query paramater.
+        data.reduce(
+          (acc, slug) => ({
+            '/': { page: '/' },
+            ...acc,
+            [`/posts/${slug}`]: { page: '/post', query: { slug } }
+          }),
+          {}
+        )
+      )
+      .catch(console.error)
+    return path
   }
 })
