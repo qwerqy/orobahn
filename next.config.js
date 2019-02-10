50c34b423228e70f57761559a75c9d3ede911d99
Amin Roslan <amnrsln@gmail.com>
2019-02-11 00:43:04 +0800

    test

--- next.config.js
+++ next.config.js
@@ -11,30 +11,30 @@ module.exports = withCSS({
     };
 
     return config;
-  },
-  async exportPathMap() {
-    const butter = Butter("fd1efe394a6740dbfe76ff507508849f406c2aca");
-    // we fetch our list of posts, this allow us to dynamically generate the exported pages
-    // will need to modify this if page gets more posts
-    const response = await butter.post.list({ page: 1, page_size: 10 });
-    const posts = response.data;
-
-    // tranform the list of posts into a map of pages with the pathname `/post/:id`
-    const pages = posts.data.reduce(
-      (pages, post) =>
-        Object.assign({}, pages, {
-          [`/post/${post.slug}`]: {
-            page: "/post",
-            query: { title: post.slug }
-          }
-        }),
-      {}
-    );
-    // Combine pages with other pages in project.
-    return Object.assign({}, pages, {
-      "/": { page: "/" },
-      "/blog": { page: "/blog" },
-      "/software-portfolio": { page: "/software-portfolio" }
-    });
   }
+  // async exportPathMap() {
+  //   const butter = Butter("fd1efe394a6740dbfe76ff507508849f406c2aca");
+  //   // we fetch our list of posts, this allow us to dynamically generate the exported pages
+  //   // will need to modify this if page gets more posts
+  //   const response = await butter.post.list({ page: 1, page_size: 10 });
+  //   const posts = response.data;
+
+  //   // tranform the list of posts into a map of pages with the pathname `/post/:id`
+  //   const pages = posts.data.reduce(
+  //     (pages, post) =>
+  //       Object.assign({}, pages, {
+  //         [`/post/${post.slug}`]: {
+  //           page: "/post",
+  //           query: { title: post.slug }
+  //         }
+  //       }),
+  //     {}
+  //   );
+  //   // Combine pages with other pages in project.
+  //   return Object.assign({}, pages, {
+  //     "/": { page: "/" },
+  //     "/blog": { page: "/blog" },
+  //     "/software-portfolio": { page: "/software-portfolio" }
+  //   });
+  // }
 });
