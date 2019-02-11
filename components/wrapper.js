55247caca65795ec10c828753e5c35f22163bbf4
Amin Roslan <amnrsln@gmail.com>
2019-02-11 19:55:03 +0800

    prettier run

--- components/wrapper.js
+++ components/wrapper.js
@@ -1,97 +1,133 @@
 import { Component, Fragment } from "react";
 import Link from "next/link";
-import { Transition, Segment, Container, Menu, Icon, Sidebar, Visibility } from "semantic-ui-react";
+import {
+  Transition,
+  Segment,
+  Container,
+  Menu,
+  Icon,
+  Sidebar,
+  Visibility
+} from "semantic-ui-react";
 
 import "../assets/nav.css";
 
 const links = [
   { label: "blog", href: "/blog" },
-  { label: "software portfolio", href: "/software-portfolio"},
+  { label: "software portfolio", href: "/software-portfolio" }
   // { label: "gaming", href: "/gaming" }
 ];
 
 class DesktopWrapper extends Component {
   state = {
     fixedNav: false
-  }
+  };
 
   getPosition = () => {
-    const { fixedNav } = this.state
-    const { solid } = this.props
+    const { fixedNav } = this.state;
+    const { solid } = this.props;
     if (solid) {
-      return 'relative'
+      return "relative";
     } else if (fixedNav) {
-      return 'fixed'
+      return "fixed";
     } else {
-      return 'absolute'
+      return "absolute";
     }
-  }
+  };
 
   getBackgroundColor = () => {
-    const { fixedNav } = this.state
-    const { solid } = this.props
+    const { fixedNav } = this.state;
+    const { solid } = this.props;
 
     if (fixedNav) {
-      return 'white'
+      return "white";
     } else if (solid) {
-      return '#1b1c1d'
+      return "#1b1c1d";
     } else {
-      return 'transparent'
+      return "transparent";
     }
-  }
+  };
 
-  pathnameCleaner =() => {
-    let result = this.props.url.pathname
-    if (this.props.url.pathname.includes('-')) {
-      result = result.replace('-', ' ')
+  pathnameCleaner = () => {
+    let result = this.props.url.pathname;
+    if (this.props.url.pathname.includes("-")) {
+      result = result.replace("-", " ");
     }
-    result = result.replace('/','')
-    
-    return result 
-  }
+    result = result.replace("/", "");
+
+    return result;
+  };
 
   render() {
     const { dark } = this.props;
     const { fixedNav } = this.state;
-    const pathname = this.pathnameCleaner()
+    const pathname = this.props.url.pathname ? this.pathnameCleaner() : "";
     const styles = {
       segment: {
         backgroundColor: this.getBackgroundColor(),
-        border: 0, 
+        border: 0,
         position: this.getPosition(),
         zIndex: "100",
         margin: 0,
-        boxShadow: fixedNav ? '0 4px 8px 0 rgba(0, 0, 0, 0.2)' : '',
-        width: '100vw',
+        boxShadow: fixedNav ? "0 4px 8px 0 rgba(0, 0, 0, 0.2)" : "",
+        width: "100vw",
         top: 0
       }
     };
 
     return (
-      <div className='desktop-wrapper'>
-        <Visibility once={false} onBottomPassed={() => this.setState({ fixedNav: true })} onBottomPassedReverse={() => this.setState({ fixedNav: false })}>
+      <div className="desktop-wrapper">
+        <Visibility
+          once={false}
+          onBottomPassed={() => this.setState({ fixedNav: true })}
+          onBottomPassedReverse={() => this.setState({ fixedNav: false })}
+        >
           {/* <Transition visible={} animation="fade down" duration={500}> */}
-            <Segment inverted vertical textAlign='center' style={styles.segment}>
-              <Container text>
-                <Menu inverted={dark && !fixedNav} pointing secondary style={{ borderBottom: 0 }}>
-                  <Link href='/'>
-                    <Menu.Item as='a' className="nav-header" style={{letterSpacing: "3px"}} header>AMIN ROSLAN</Menu.Item>
-                  </Link>
-                  <Menu.Menu className='right-menu' position='right'>
-                    {
-                      Object.keys(links).map( i => {
-                        return (
-                          <Link key={i} href={links[i].href}>
-                            <Menu.Item as='a' className='nav-item' key={i} name={links[i].label} active={links[i].label === pathname} onClick={this.handleClick} position={links[i].position} />
-                          </Link>
-                        );
-                      })
-                    }
-                  </Menu.Menu>
-                  <Menu.Item className="dropper-right" position='right' onClick={this.handleClickToToggle}><Icon name="sidebar"/></Menu.Item>
-                </Menu>
-              </Container>
-            </Segment>
+          <Segment inverted vertical textAlign="center" style={styles.segment}>
+            <Container text>
+              <Menu
+                inverted={dark && !fixedNav}
+                pointing
+                secondary
+                style={{ borderBottom: 0 }}
+              >
+                <Link href="/">
+                  <Menu.Item
+                    as="a"
+                    className="nav-header"
+                    style={{ letterSpacing: "3px" }}
+                    header
+                  >
+                    AMIN ROSLAN
+                  </Menu.Item>
+                </Link>
+                <Menu.Menu className="right-menu" position="right">
+                  {Object.keys(links).map(i => {
+                    return (
+                      <Link key={i} href={links[i].href}>
+                        <Menu.Item
+                          as="a"
+                          className="nav-item"
+                          key={i}
+                          name={links[i].label}
+                          active={links[i].label === pathname}
+                          onClick={this.handleClick}
+                          position={links[i].position}
+                        />
+                      </Link>
+                    );
+                  })}
+                </Menu.Menu>
+                <Menu.Item
+                  className="dropper-right"
+                  position="right"
+                  onClick={this.handleClickToToggle}
+                >
+                  <Icon name="sidebar" />
+                </Menu.Item>
+              </Menu>
+            </Container>
+          </Segment>
           {/* </Transition> */}
         </Visibility>
         {this.props.children}
@@ -100,101 +136,129 @@ class DesktopWrapper extends Component {
   }
 }
 
-class MobileWrapper extends Component{
+class MobileWrapper extends Component {
   state = {
     visible: false
-  }
+  };
 
   getPosition = () => {
-    const { solid } = this.props
+    const { solid } = this.props;
     if (solid) {
-      return 'relative'
+      return "relative";
     } else {
-      return 'absolute'
+      return "absolute";
     }
-  }
+  };
 
   getBackgroundColor = () => {
-    const { solid } = this.props
+    const { solid } = this.props;
 
     if (solid) {
-      return '#1b1c1d'
+      return "#1b1c1d";
     } else {
-      return 'transparent'
+      return "transparent";
     }
-  }
+  };
 
-  handleClickToToggle = () => this.setState({ visible: true })
+  handleClickToToggle = () => this.setState({ visible: true });
 
-  handleSidebarHide = () => this.setState({ visible: false })
+  handleSidebarHide = () => this.setState({ visible: false });
 
-  pathnameCleaner =() => {
-    let result = this.props.url.pathname
-    if (this.props.url.pathname.includes('-')) {
-      result = result.replace('-', ' ')
+  pathnameCleaner = () => {
+    let result = this.props.url.pathname;
+    if (this.props.url.pathname.includes("-")) {
+      result = result.replace("-", " ");
     }
 
-    result.replace('/','')
-    
-    return result 
-  }
+    result.replace("/", "");
+
+    return result;
+  };
   render() {
     const { activeItem, visible } = this.state;
-    const pathname = this.pathnameCleaner()
+    const pathname = this.pathnameCleaner();
 
     const styles = {
       segment: {
         backgroundColor: this.getBackgroundColor(),
-        border: 0, 
+        border: 0,
         position: this.getPosition(),
         zIndex: "100",
         margin: 0,
-        width: '100%',
+        width: "100%",
         top: 0
       },
       sidebarItem: {
-        padding: '20px 10px',
-        letterSpacing: '3px',
+        padding: "20px 10px",
+        letterSpacing: "3px",
         lineHeight: 1.5
       }
     };
 
     return (
-      <Sidebar.Pushable className='mobile-wrapper'>
+      <Sidebar.Pushable className="mobile-wrapper">
         <Sidebar
           as={Menu}
-          style={{padding:'0px 5px'}}
-          animation='overlay'
-          icon='labeled'
+          style={{ padding: "0px 5px" }}
+          animation="overlay"
+          icon="labeled"
           inverted
-          direction='right'
+          direction="right"
           onHide={this.handleSidebarHide}
           vertical
           visible={visible}
-          width='thin'
+          width="thin"
         >
-          <Menu.Item style={styles.sidebarItem} onClick={ () => this.setState({ visible: false })} header><Icon name='angle right'/></Menu.Item>
-          {
-            Object.keys(links).map( i => {
-              return (
-                <Link key={i} href={links[i].href}>
-                  <Menu.Item as='a' style={styles.sidebarItem} key={i} name={links[i].label} active={links[i].label === pathname || links[i].label.includes(pathname)} onClick={this.handleClick} position={links[i].position} />
-                </Link>
-              );
-            })
-          }
+          <Menu.Item
+            style={styles.sidebarItem}
+            onClick={() => this.setState({ visible: false })}
+            header
+          >
+            <Icon name="angle right" />
+          </Menu.Item>
+          {Object.keys(links).map(i => {
+            return (
+              <Link key={i} href={links[i].href}>
+                <Menu.Item
+                  as="a"
+                  style={styles.sidebarItem}
+                  key={i}
+                  name={links[i].label}
+                  active={
+                    links[i].label === pathname ||
+                    links[i].label.includes(pathname)
+                  }
+                  onClick={this.handleClick}
+                  position={links[i].position}
+                />
+              </Link>
+            );
+          })}
         </Sidebar>
         <Sidebar.Pusher dimmed={visible}>
-            <Segment inverted vertical textAlign='center' style={styles.segment}>
-              <Container text>
-                <Menu inverted pointing secondary style={{ borderBottom: 0 }}>
-                  <Link href='/'>
-                    <Menu.Item as='a' className="nav-header" style={{letterSpacing: "3px"}} header>AMIN ROSLAN</Menu.Item>
-                  </Link>
-                  <Menu.Item className="dropper-right" position='right' onClick={this.handleClickToToggle}><Icon name="sidebar"/></Menu.Item>
-                </Menu>
-              </Container>
-            </Segment>
+          <Segment inverted vertical textAlign="center" style={styles.segment}>
+            <Container text>
+              <Menu inverted pointing secondary style={{ borderBottom: 0 }}>
+                <Link href="/">
+                  <Menu.Item
+                    as="a"
+                    className="nav-header"
+                    style={{ letterSpacing: "3px" }}
+                    header
+                  >
+                    AMIN ROSLAN
+                  </Menu.Item>
+                </Link>
+                <Menu.Item
+                  className="dropper-right"
+                  position="right"
+                  onClick={this.handleClickToToggle}
+                >
+                  <Icon name="sidebar" />
+                </Menu.Item>
+              </Menu>
+            </Container>
+          </Segment>
           {this.props.children}
         </Sidebar.Pusher>
       </Sidebar.Pushable>
