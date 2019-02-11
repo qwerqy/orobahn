0d30e9a7925ddce9bef54d47f86e8fe4faef54e8
Amin Roslan <amnrsln@gmail.com>
2019-02-11 19:57:18 +0800

    freelance initial page

---
+++ pages/freelance.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Freelance extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Freelance page.</h1>
+      </div>
+    );
+  }
+}
+
+export default Freelance;
