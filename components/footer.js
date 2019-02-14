409692b5f87e0a1fd8b5e28236250f9b44cf4633
Amin Roslan <amnrsln@gmail.com>
2019-02-14 23:59:04 +0800

    add link to photo owner

--- components/footer.js
+++ components/footer.js
@@ -71,7 +71,7 @@ class Footer extends Component {
                   </List.Item>
                 </List>
               </Grid.Column>
-              <Grid.Column width={7}>
+              <Grid.Column width={4}>
                 <Header as="h4" inverted>
                   Social Media
                 </Header>
@@ -104,6 +104,51 @@ class Footer extends Component {
                   onClick={() => gaUserTracking("Footer", `Clicked ButterCMS`)}
                 >
                   <Image size="small" src="/static/butter-w.png" />
+                </a>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header as="h4" inverted>
+                  Background Image by
+                </Header>
+                <a
+                  style={{
+                    backgroundColor: "#252525",
+                    color: "white",
+                    textDecoration: "none",
+                    padding: "4px 6px",
+                    fontFamily:
+                      '-apple-system, BlinkMacSystemFont, "San Francisco", "Helvetica Neue", Helvetica, Ubuntu, Roboto, Noto, "Segoe UI", Arial, sans-serif',
+                    fontSize: "12px",
+                    fontWeight: "bold",
+                    lineHeight: 1.2,
+                    display: "inline-block",
+                    borderRadius: "3px"
+                  }}
+                  href="https://unsplash.com/@adrienolichon?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Download free do whatever you want high-resolution photos from Adrien Olichon"
+                >
+                  <span style={{ display: "inline-block", padding: "2px 3px" }}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{
+                        height: "12px",
+                        width: "auto",
+                        position: "relative",
+                        verticalAlign: "middle",
+                        top: "-2px",
+                        fill: "white"
+                      }}
+                      viewBox="0 0 32 32"
+                    >
+                      <title>unsplash-logo</title>
+                      <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z" />
+                    </svg>
+                  </span>
+                  <span style={{ display: "inline-block", padding: "2px 3px" }}>
+                    Adrien Olichon
+                  </span>
                 </a>
               </Grid.Column>
             </Grid.Row>
