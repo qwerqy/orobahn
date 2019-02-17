bbec3e94bf8c0f9973c0f826a749cfc41aa234bc
Amin Roslan <amnrsln@gmail.com>
2019-02-17 20:56:34 +0800

    [Feat] Initial integration of service worker

--- next.config.js
+++ next.config.js
@@ -1,7 +1,40 @@
 const withCSS = require("@zeit/next-css");
+const withOffline = moduleExists("next-offline") ? require("next-offline") : {};
 
-module.exports = withCSS({
+const nextConfig = {
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "networkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  },
   webpack(config, options) {
     return config;
   }
-});
+};
+
+module.exports = moduleExists("next-offline")
+  ? withOffline(withCSS(nextConfig))
+  : withCSS(nextConfig);
+
+function moduleExists(name) {
+  try {
+    return require.resolve(name);
+  } catch (error) {
+    return false;
+  }
+}
