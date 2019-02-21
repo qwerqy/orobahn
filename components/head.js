86b9e279262a4f7eeb045e51476e1dbfef6ba1e0
Amin Roslan <amnrsln@gmail.com>
2019-02-21 20:32:46 +0800

    [Feature] Better looking favicon. audit performances, etc

--- components/head.js
+++ components/head.js
@@ -10,6 +10,30 @@ const Head = props => (
   <Fragment>
     <NextHead>
       <meta charSet="UTF-8" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/icons/apple-touch-icon.png"
+      />
+      >
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/icons/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="/static/icons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#1b1c1d" />
       <title>{props.title || ""}</title>
       <meta
